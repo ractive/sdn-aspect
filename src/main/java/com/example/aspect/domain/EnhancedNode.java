becc48dfb4619bc053ead33b924115352ee62a17
Jean-Pierre Bergamin <james@ractive.ch>
2012-01-07 16:44:04 +0100

    Javadoc

--- src/main/java/com/example/aspect/domain/EnhancedNode.java
+++ src/main/java/com/example/aspect/domain/EnhancedNode.java
@@ -1,5 +1,9 @@
 package com.example.aspect.domain;
 
+/**
+ * @NodeEntity classes can be "enhanced" with methods from this interface
+ * The EnhancesNodeApsect adds the implementations.
+ */
 public interface EnhancedNode  {
 	public void saveMe();
 }
--- src/main/java/com/example/aspect/domain/EnhancedNodeEntity.java
+++ src/main/java/com/example/aspect/domain/EnhancedNodeEntity.java
@@ -1,5 +1,9 @@
 package com.example.aspect.domain;
 
+/**
+ * @NodeEntity classes can have this annotation as well to have the interface EnhancedNode its
+ * implementation in EnhancedNodeAspect applied.
+ */
 public @interface EnhancedNodeEntity {
 
 }
